c89f2fe9363a08692b1d8d67aab3e937c42d2a97
Dylan Piersanti <dylngae@outlook.com>
2019-11-09 14:29:21 +0100

    FirstCommit (create environnement)

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+    <script src="assets/style/js/jquery.min.js"></script>
+    <script src="assets/style/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php require('utils/db.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style/css/style.css">
+    <link rel="stylesheet" href="assets/style/css/all.min.css">
+</head>
+
+<body>
